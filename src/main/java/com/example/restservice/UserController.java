d1a88ba916bcb5422e6766cfd3c01548dc7bdfaa
Viktoria Ivanova <vika_ivanova290@mail.ru>
2022-12-20 17:46:42 +0500

    Initial commit

---
+++ src/main/java/com/example/restservice/UserController.java
@@ -0,0 +1,21 @@
+package com.example.restservice;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("user")
+public class UserController {
+    @GetMapping
+    User getUser(){
+        return new User("jojo","bean");
+    }
+    @PostMapping
+    UserPost postUser(@RequestBody User user){
+        LocalDateTime localDate = LocalDateTime.now();
+        UserPost userPost = new UserPost(user.getLogin(), localDate.toString());
+        return userPost;
+    }
+}
